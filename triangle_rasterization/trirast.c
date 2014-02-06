ad04eb57e7931487e7e4e3840518aefb05f37ff6
youpii <juylings@hotmail.com>
2014-02-06 14:54:28 +0100

    driehoekjes

--- triangle_rasterization/trirast.c
+++ triangle_rasterization/trirast.c
@@ -26,10 +26,74 @@
  * The triangle is drawn in color (r,g,b).
  */
 
+float f01(float x0,float x1,float y0,float y1,float x,float y)
+{
+	float f01;
+	f01 = (y0 - y1) * x + (x1 - x0) * y + (x0*y1 - x1*y0);
+	return f01;
+
+}
+
+float f12(float x1,float y1,float x2,float y2,float x,float y)
+{
+	float f12;
+	f12 = (y1 - y2) * x + (x2 - x1) * y + (x1*y2 - x2*y1);
+	return f12;
+
+}
+
+float f20(float x0,float y0,float x2,float y2,float x,float y)
+{	
+	float f20;
+	f20 = (y2 - y0) * x + (x0 - x2) * y + (x2*y0 - x0*y2);
+	return f20;
+}
+
+
 void
 draw_triangle(float x0, float y0, float x1, float y1, float x2, float y2,
     byte r, byte g, byte b)
 {
+
+		float xmin, xtemp_min, xtemp_max, ytemp_min, ytemp_max, xmax, ymin, ymax, alpha, betha, epsilon, fa, fb, fj;
+		byte red = r, green = g, blue = b;
+		int colour;
+
+		/*calculates the maximim and minumim values */
+		xtemp_min = min(x0,x1);
+		xmin = min(xtemp_min, x2);
+
+		xtemp_max = max(x0, x1);
+		xmax = max(xtemp_max, x2);
+
+		ytemp_min = min(y0, y1);
+		ymin = min(ytemp_min, y2);
+
+		ytemp_max = max(y0, y1);
+		ymax = max(ytemp_max, y2);
+
+		fa = f12(x0, y0);
+		fb = f20(x1, y1);
+		fj = f01(x2, y2);
+
+		for (y = ymin; ymin < ymax; y++)
+		{
+			for (x = xmin; xmin < xmax; x++)
+			{
+				alpha = (f12(x, y) / fa);
+				betha = (f20(x, y) / fb);
+				epsilon = (f01(x, y) / fj);
+				if (alpha >= 0 && alpha >= 0 && epsilon >= 0)
+				{
+					if ((alpha > 0 || fa(-1,-1) > 0) && (betha > 0 || fb(-1,-1) > 0) && (epsilon > 0 || fj(-1,-1) > 0)
+					{
+						colour = (alpha*red + betha*green + epsilon*blue);
+					    PutPixel(x,y,colour);
+					}
+				}
+			}
+		}
+
 }
 
 void
--- triangle_rasterization/trirast.h
+++ triangle_rasterization/trirast.h
@@ -8,5 +8,11 @@ void    draw_triangle(float x0, float y0, float x1, float y1,
 void    draw_triangle_optimized(float x0, float y0, float x1, float y1,
             float x2, float y2, byte r, byte g, byte b);
 
+float f01(float x0,float x1,float y0,float y1,float x,float y);
+float f12(float x1,float y1,float x2,float y2,float x,float y);
+float f20(float x0,float y0,float x2,float y2,float x,float y);
+
+
+
 #endif
 
