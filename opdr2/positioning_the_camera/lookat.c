c2ed88bcdefc3b3ee5746bf11934e6de06df3bc1
Manuel Overdijk <manuel@ubuntu.(none)>
2014-02-11 06:53:27 -0800

    its a wrap!

--- opdr2/positioning_the_camera/lookat.c
+++ opdr2/positioning_the_camera/lookat.c
@@ -23,7 +23,28 @@
 #define M_PI           3.14159265358979323846  /* pi */
 #endif
 
+void normalize(GLfloat *a)
+{
+    int i = 0;
+    GLfloat len = sqrt(inProduct(a, a));
+    for(; i < 3; i++)
+    {
+        a[i] /= len;
+    }
+}
+
+
 void myLookAt(GLdouble eyeX, GLdouble eyeY, GLdouble eyeZ,
               GLdouble centerX, GLdouble centerY, GLdouble centerZ,
               GLdouble upX, GLdouble upY, GLdouble upZ) {
+
+	GLdouble cz[3]
+	cz[0] = eyeX - centerX;
+	cz[1] = eyeY - centerY;
+	cz[2] = eyeZ - centerZ;
+
+
+	normalize(cz);
+
+	
 }
