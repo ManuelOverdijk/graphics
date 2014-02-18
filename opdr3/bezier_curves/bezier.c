2d6fcfb644a69ae65eb61f451476482ed8317d98
youpii <juylings@hotmail.com>
2014-02-18 12:53:30 +0100

    deel 2

--- opdr3/bezier_curves/bezier.c
+++ opdr3/bezier_curves/bezier.c
@@ -26,11 +26,43 @@
  * respectively.
  */
 
+
+GLfloat binomial(GLint n, GLint k) {
+	int nk = n - k;
+
+    GLint value_n = 1;
+    for (GLint i = 2; i <= n; i++) {
+        value_n *= i;
+    }
+
+    GLint value_k = 1;    
+    for (GLint i = 2; i <= k; i++) {
+    value_k *= i;}
+
+    GLint n_min_k = 1;    
+    for (GLint i = 2; i <= nk; i++) {
+    n_min_k *= i;    
+    }
+    return value_n / ((GLfloat) value_k * n_min_k);
+}
+
+/* Calculate the ith Bernstein polynomial of degree n
+ */
+GLfloat bernstein(GLint n, GLint k, GLfloat u) {
+    return binomial(n, k) * pow(u, k) * pow(1 - u, n - k);
+}
+
 void
-evaluate_bezier_curve(float *x, float *y, control_point p[], int num_points, float u)
+evaluate_bezier_curve(GLfloat *x, GLfloat *y, control_point p[], GLint num_points, GLfloat u)
 {
     *x = 0.0;
     *y = 0.0;
+
+        for (GLint i = 0; i < num_points; i++) {
+        GLfloat bern = bernstein(num_points - 1, i, u);
+        *x += bern * p[i].x;
+        *y += bern * p[i].y;
+    }
 }
 
 /* Draw a Bezier curve defined by the control points in p[], which
@@ -55,8 +87,26 @@ evaluate_bezier_curve(float *x, float *y, control_point p[], int num_points, flo
  */
 
 void
-draw_bezier_curve(int num_segments, control_point p[], int num_points)
+draw_bezier_curve(GLint num_segments, control_point p[], GLint num_points)
 {
+
+    GLfloat x, y;
+
+    glBegin(GL_LINE_STRIP);
+
+    // start line
+    glVertex2f(p[0].x, p[0].y);
+
+    // calculate and draw points in the middle
+    for (GLint i = 1; i < num_segments; i++) {
+        evaluate_bezier_curve(&x, &y, p, num_points, i / (GLfloat) num_segments);
+        glVertex2f(x, y);
+    }
+
+    // end line
+    glVertex2f(p[num_points - 1].x, p[num_points - 1].y);
+    glEnd();
+
 }
 
 /* Find the intersection of a cubic Bezier curve with the line X=x.
--- opdr3/bezier_curves/bezier.h
+++ opdr3/bezier_curves/bezier.h
@@ -11,6 +11,11 @@ typedef struct control_point
 }
 control_point;
 
+int factorial(int n);
+float binomial(int n, int k);
+float bernstein(int n, int i, float u);
+
+
 void    evaluate_bezier_curve(float *x, float *y, control_point p[], int num_points, float u);
 void    draw_bezier_curve(int num_segments, control_point p[], int num_points);
 int     intersect_cubic_bezier_curve(float *y, control_point p[], float x);
